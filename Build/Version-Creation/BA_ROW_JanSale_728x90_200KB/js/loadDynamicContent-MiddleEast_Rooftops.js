94523b7f33e35587d5526866e16ec079b51865bb
Joel Fernandes <joel.fernandes@hogarthww.com>
2020-12-15 17:10:03 +0000

    Add : India - Friends & Family and Nearest & Dearest. Middle East - Rooftops & Clifftops and Cuppas & Long lunches

---
+++ Build/Version-Creation/BA_ROW_JanSale_728x90_200KB/js/loadDynamicContent-MiddleEast_Rooftops.js
@@ -0,0 +1,102 @@
+var devDynamicContent;
+
+(function () {
+
+	var _feedName = "TBC";
+	devDynamicContent = {};
+
+	devDynamicContent[_feedName] = [{}];
+	devDynamicContent[_feedName][0]._id = 0;
+	devDynamicContent[_feedName][0].Unique_ID = 30002;
+	devDynamicContent[_feedName][0].Default = false;
+	devDynamicContent[_feedName][0].Active = true;
+	devDynamicContent[_feedName][0].Exit_URL = {};
+	devDynamicContent[_feedName][0].Exit_URL.Url = "http://www.ba.com";
+	devDynamicContent[_feedName][0].Reporting_Label = "RoofTops_CliffTops_728x90";
+
+	devDynamicContent[_feedName][0].frame01a_img = {};
+	devDynamicContent[_feedName][0].frame01a_img.Type = "file";
+	devDynamicContent[_feedName][0].frame01a_img.Url = "../_assets/img_rooftops_282x78_x2.jpg";
+	devDynamicContent[_feedName][0].frame01b_img = {};
+	devDynamicContent[_feedName][0].frame01b_img.Type = "file";
+	devDynamicContent[_feedName][0].frame01b_img.Url = "../_assets/img_clifftops_282x78_x2.jpg";
+	devDynamicContent[_feedName][0].frame01_copy01 = "TAKE OFF TO";
+	devDynamicContent[_feedName][0].frame01_copy02 = "ROOFTOPS<div>CLIFFTOPS</div>";
+	devDynamicContent[_feedName][0].frame01_icon = {};
+	devDynamicContent[_feedName][0].frame01_icon.Type = "file";
+	devDynamicContent[_feedName][0].frame01_icon.Url = "../_assets/ampersand_728x90_x2.gif";
+	devDynamicContent[_feedName][0].frame01_frame = {};
+	devDynamicContent[_feedName][0].frame01_frame.Type = "file";
+	devDynamicContent[_feedName][0].frame01_frame.Url = "../_assets/img_frame_Jansale2021_728x90.png";
+
+	devDynamicContent[_feedName][0].frame02_copy01 = "";
+	devDynamicContent[_feedName][0].frame02_copy02 = "";
+	devDynamicContent[_feedName][0].frame02_img = {};
+	devDynamicContent[_feedName][0].frame02_img.Type = "file";
+	devDynamicContent[_feedName][0].frame02_img.Url = "../_assets/noImage.png";
+
+	devDynamicContent[_feedName][0].frame03_copy01 = "<div id='frame03_copy01a' style='right: 168px;'>BOOK NOW</div><div id='frame03_copy01b' style='left: 175px;' >CHANGE LATER</div>";
+	devDynamicContent[_feedName][0].frame03_copy02 = "WITH OUR FLEXIBLE BOOKING OPTIONS";
+	devDynamicContent[_feedName][0].frame03_img = {};
+	devDynamicContent[_feedName][0].frame03_img.Type = "file";
+	devDynamicContent[_feedName][0].frame03_img.Url = "../_assets/noImage.png";
+
+	devDynamicContent[_feedName][0].frame04_copy01 = "GREAT FARES TO";
+	devDynamicContent[_feedName][0].frame04_copy02 = "LONDON<div>BEYOND</div>";
+	devDynamicContent[_feedName][0].copy_frame04_from = "";
+	devDynamicContent[_feedName][0].copy_frame04_currency = "";
+	devDynamicContent[_feedName][0].copy_frame04_price = "";
+	devDynamicContent[_feedName][0].copy_frame04_pp = "";
+	devDynamicContent[_feedName][0].copy_frame04_package = "";
+	devDynamicContent[_feedName][0].copy_frame04_ends = "";
+
+	devDynamicContent[_feedName][0].endframe_copy01 = "";
+	devDynamicContent[_feedName][0].endframe_copy02 = "";
+
+	devDynamicContent[_feedName][0].cta_copy = "BOOK NOW";
+
+	devDynamicContent[_feedName][0].terms_frame01 = "";
+	devDynamicContent[_feedName][0].terms_frame02 = "";
+	devDynamicContent[_feedName][0].terms_frame03 = "Terms apply.";
+	devDynamicContent[_feedName][0].terms_frame04 = "Terms apply.";
+
+	devDynamicContent[_feedName][0].frameTimes = "4,0,4";
+
+	//    0:F1a_img, 1:F1b_img, 2:F1_icon, 3:F1_1, 4:F1_2, 5:F2_1, 6:F2_2, 7:F2_i, 8:F3_1  9:F3_2  10:F3_i 11:Fe_1 12:Fe_2   13:F4_1, 14:F4_2  15:F4_p  16:cta
+	devDynamicContent[_feedName][0].showCopyDelays="0,0,.5,.5,.5,1,1,.5,.5,.5,.5,0,0,.5,.5,.5,1";
+
+	//   0:F1a_img, 1:F1b_img, 2:F1_icon, 3:F1_1, 4:F1_2, 5:F2_1, 6:F2_2, 7:F2_i, 8:F3_1  9:F3_2  10:F3_i 11:Fe_1 12:Fe_2   13:F4_1, 14:F4_2,  15:F4_p  16:cta
+	devDynamicContent[_feedName][0].copyAnimTypes="10.5,11.5,12,f,f,f,f,f,f,f,f,f,f,f,f,f,99"
+
+	devDynamicContent[_feedName][0].logoBA01Frames = "true,true,true,true";
+	devDynamicContent[_feedName][0].logoBA02Frames = "false,false,false,false";
+	devDynamicContent[_feedName][0].logoPartnersFrames = "false,false,true,true";
+	devDynamicContent[_feedName][0].logoExtraFrames = "false,false,false,false";
+
+	devDynamicContent[_feedName][0].img_logo_ba_01 = {};
+	devDynamicContent[_feedName][0].img_logo_ba_01.Type = "file";
+	devDynamicContent[_feedName][0].img_logo_ba_01.Url = "../_assets/logo_BA_Jansale2021_728x90_x2.png";
+	devDynamicContent[_feedName][0].img_logo_ba_02 = {};
+	devDynamicContent[_feedName][0].img_logo_ba_02.Type = "file";
+	devDynamicContent[_feedName][0].img_logo_ba_02.Url = "../_assets/noImage.png";
+	devDynamicContent[_feedName][0].img_logo_partners = {};
+	devDynamicContent[_feedName][0].img_logo_partners.Type = "file";
+	devDynamicContent[_feedName][0].img_logo_partners.Url = "../_assets/logo_BA_partners_noATOL_728x90_x2.png";
+	devDynamicContent[_feedName][0].img_logo_extra = {};
+	devDynamicContent[_feedName][0].img_logo_extra.Type = "file";
+	devDynamicContent[_feedName][0].img_logo_extra.Url = "../_assets/noImage.png";
+	devDynamicContent[_feedName][0].img_replayBtn = {};
+	devDynamicContent[_feedName][0].img_replayBtn.Type = "file";
+	devDynamicContent[_feedName][0].img_replayBtn.Url = "../_assets/btnReplay_x2.png";
+
+	devDynamicContent[_feedName][0].loopingProps = "0,4,true";
+	devDynamicContent[_feedName][0].css_overrides = "#frame04_copy01, #frame04_copy02, #frame03_copy01 {color:#1255a3;} #frame01_copy01 { left: 129px; text-align: left; letter-spacing: 1.2px; font-size: 11px; top:11px;} #frame01_copy02 { left: 73px; letter-spacing: 3.4px; } #frame01_copy02 div { left: 241px; letter-spacing: 3.9px; } #frame01_copy01,#frame01_copy02{text-shadow:0 0 4px rgba(0,0,0,0.3)} #frame03_copy01 { font-size: 26px; letter-spacing: 2.8px; left: -60px; } #frame03_copy01 div { display: inline-block; margin-left: 42px; } #frame04_copy01,#frame04_copy02{font-size:26px;line-height:27px;letter-spacing:2px;top:32px;left:18px;width:275px} #frame04_copy02 { left: 67px; } #frame04_copy02 div {position:absolute; top:0; left:247px;} #cta { top: 34px; left: 481px; transform: scale(1.1)!important; } .termsCopy { right: 8px; bottom: 6px; left: auto; text-align: right; } #frame01a_img_container,#frame01b_img_container{width:282px;height:78px;left:288px;top:6px;} #frame01a_img_container{left:6px;} #frame01_icon.animateOff_F3{opacity:1!important} #frame01.animateOff{transition:opacity .5s 0s} #frame04_copy01{font-size:11px;top:2px;left:150px;letter-spacing:1.5px}";
+
+	try {
+		Enabler.setDevDynamicContent(devDynamicContent);
+	} catch (error) {
+		// console.log(error)
+	}
+
+	return [devDynamicContent, _feedName];
+})();
---
+++ Build/Version-Creation/BA_ROW_JanSale_970x250_200KB/js/loadDynamicContent-default.js
@@ -0,0 +1,102 @@
+var devDynamicContent;
+
+(function () {
+
+	var _feedName = "TBC";
+	devDynamicContent = {};
+
+	devDynamicContent[_feedName] = [{}];
+	devDynamicContent[_feedName][0]._id = 0;
+	devDynamicContent[_feedName][0].Unique_ID = 40000;
+	devDynamicContent[_feedName][0].Default = false;
+	devDynamicContent[_feedName][0].Active = true;
+	devDynamicContent[_feedName][0].Exit_URL = {};
+	devDynamicContent[_feedName][0].Exit_URL.Url = "http://www.ba.com";
+	devDynamicContent[_feedName][0].Reporting_Label = "Default_970x250";
+
+	devDynamicContent[_feedName][0].frame01a_img = {};
+	devDynamicContent[_feedName][0].frame01a_img.Type = "file";
+	devDynamicContent[_feedName][0].frame01a_img.Url = "images/noImage.png";
+	devDynamicContent[_feedName][0].frame01b_img = {};
+	devDynamicContent[_feedName][0].frame01b_img.Type = "file";
+	devDynamicContent[_feedName][0].frame01b_img.Url = "images/noImage.png";
+	devDynamicContent[_feedName][0].frame01_copy01 = "";
+	devDynamicContent[_feedName][0].frame01_copy02 = "";
+	devDynamicContent[_feedName][0].frame01_icon = {};
+	devDynamicContent[_feedName][0].frame01_icon.Type = "file";
+	devDynamicContent[_feedName][0].frame01_icon.Url = "images/noImage.png";
+	devDynamicContent[_feedName][0].frame01_frame = {};
+	devDynamicContent[_feedName][0].frame01_frame.Type = "file";
+	devDynamicContent[_feedName][0].frame01_frame.Url = "images/noImage.png";
+
+	devDynamicContent[_feedName][0].frame02_copy01 = "";
+	devDynamicContent[_feedName][0].frame02_copy02 = "";
+	devDynamicContent[_feedName][0].frame02_img = {};
+	devDynamicContent[_feedName][0].frame02_img.Type = "file";
+	devDynamicContent[_feedName][0].frame02_img.Url = "images/noImage.png";
+
+	devDynamicContent[_feedName][0].frame03_copy01 = "";
+	devDynamicContent[_feedName][0].frame03_copy02 = "";
+	devDynamicContent[_feedName][0].frame03_img = {};
+	devDynamicContent[_feedName][0].frame03_img.Type = "file";
+	devDynamicContent[_feedName][0].frame03_img.Url = "images/noImage.png";
+
+	devDynamicContent[_feedName][0].frame04_copy01 = "";
+	devDynamicContent[_feedName][0].frame04_copy02 = "";
+	devDynamicContent[_feedName][0].copy_frame04_from = "";
+	devDynamicContent[_feedName][0].copy_frame04_currency = "";
+	devDynamicContent[_feedName][0].copy_frame04_price = "";
+	devDynamicContent[_feedName][0].copy_frame04_pp = "";
+	devDynamicContent[_feedName][0].copy_frame04_package = "";
+	devDynamicContent[_feedName][0].copy_frame04_ends = "";
+
+	devDynamicContent[_feedName][0].endframe_copy01 = "";
+	devDynamicContent[_feedName][0].endframe_copy02 = "";
+
+	devDynamicContent[_feedName][0].cta_copy = "BOOK NOW";
+
+	devDynamicContent[_feedName][0].terms_frame01 = "";
+	devDynamicContent[_feedName][0].terms_frame02 = "";
+	devDynamicContent[_feedName][0].terms_frame03 = "";
+	devDynamicContent[_feedName][0].terms_frame04 = "";
+
+	devDynamicContent[_feedName][0].frameTimes = "0,0,0";
+
+												//    0:F1a_img, 1:F1b_img, 2:F1_icon, 3:F1_1, 4:F1_2, 5:F2_1, 6:F2_2, 7:F2_i, 8:F3_1  9:F3_2  10:F3_i 11:Fe_1 12:Fe_2   13:F4_1, 14:F4_2  15:F4_p  16:cta
+	devDynamicContent[_feedName][0].showCopyDelays = "0,         0,         .5,        0,      .5,     1,      1,      .5,     .5,    .5,      .5,     0,    4.5,      .5,      .5,      .5,      	0";
+												
+												//   0:F1a_img, 1:F1b_img, 2:F1_icon, 3:F1_1, 4:F1_2, 5:F2_1, 6:F2_2, 7:F2_i, 8:F3_1  9:F3_2  10:F3_i 11:Fe_1 12:Fe_2   13:F4_1, 14:F4_2  15:F4_p  16:cta
+	devDynamicContent[_feedName][0].copyAnimTypes = "10,        11,  	   12,     	  f,      f,      f,      f,      f,      f,      f,      f,      f,        f,       f,		 f,		  f,       f";
+
+	devDynamicContent[_feedName][0].logoBA01Frames = "true,true,true,true";
+	devDynamicContent[_feedName][0].logoBA02Frames = "false,false,false,false";
+	devDynamicContent[_feedName][0].logoPartnersFrames = "false,false,true,true";
+	devDynamicContent[_feedName][0].logoExtraFrames = "false,false,false,false";
+
+	devDynamicContent[_feedName][0].img_logo_ba_01 = {};
+	devDynamicContent[_feedName][0].img_logo_ba_01.Type = "file";
+	devDynamicContent[_feedName][0].img_logo_ba_01.Url = "images/logo_BA_970x250_x2.png";
+	devDynamicContent[_feedName][0].img_logo_ba_02 = {};
+	devDynamicContent[_feedName][0].img_logo_ba_02.Type = "file";
+	devDynamicContent[_feedName][0].img_logo_ba_02.Url = "images/noImage.png";
+	devDynamicContent[_feedName][0].img_logo_partners = {};
+	devDynamicContent[_feedName][0].img_logo_partners.Type = "file";
+	devDynamicContent[_feedName][0].img_logo_partners.Url = "images/noImage.png";
+	devDynamicContent[_feedName][0].img_logo_extra = {};
+	devDynamicContent[_feedName][0].img_logo_extra.Type = "file";
+	devDynamicContent[_feedName][0].img_logo_extra.Url = "images/noImage.png";
+	devDynamicContent[_feedName][0].img_replayBtn = {};
+	devDynamicContent[_feedName][0].img_replayBtn.Type = "file";
+	devDynamicContent[_feedName][0].img_replayBtn.Url = "images/noImage.png";
+
+	devDynamicContent[_feedName][0].loopingProps = "0,4,false";
+	devDynamicContent[_feedName][0].css_overrides = "#frame04_copy02, #frame03_copy01, .copy_price {color:#6db8e7;} #frame04_copy02{top: 198px;left: -3px;font-size: 28px;line-height: 30px;letter-spacing: 4px;} .copy_divider{left:-4px} #copy_frame04_package { top: 0; text-align: center; width: 300px; left: 0; font-size: 13px; line-height: 1.2em; } #copy_frame04_price { top: 43px; left: 113px; } #copy_frame04_pp { top: 73px; left: 195px; font-size: 9px; } #copy_frame04_currency { top: 55px; left: 87px; } #copy_frame04_ider { top: 50px; } #copy_frame04_from { top: 49px; left: 87px;} #frame04_price { left: 0; width: 300px; top: 306px;} #endframe_copy01 {left: 365px;top: 130px;}#logo_ba_01{left: -360px;top: -30px;} #cta {left:402px}";
+
+	try {
+		Enabler.setDevDynamicContent(devDynamicContent);
+	} catch (error) {
+		// console.log(error)
+	}
+
+	return [devDynamicContent, _feedName];
+})();
