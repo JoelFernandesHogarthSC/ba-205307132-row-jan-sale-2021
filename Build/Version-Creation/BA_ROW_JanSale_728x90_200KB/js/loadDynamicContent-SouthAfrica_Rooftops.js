edccf3679f91160d39c8cb7f25d8e6e46c42131a
Joel Fernandes <joel.fernandes@hogarthww.com>
2020-12-17 12:23:49 +0000

    Updated SA Rootftops manifest. Create SA Rooftop 728x90

--- Build/Version-Creation/BA_ROW_JanSale_728x90_200KB/js/loadDynamicContent-SouthAfrica_Rooftops.js
+++ Build/Version-Creation/BA_ROW_JanSale_728x90_200KB/js/loadDynamicContent-SouthAfrica_Rooftops.js
@@ -6,9 +6,9 @@ var devDynamicContent;
 	devDynamicContent = {};
 
 	devDynamicContent[_feedName] = [{}];
-	devDynamicContent[_feedName][0].Unique_ID = 40003;
-	devDynamicContent[_feedName][0].Reporting_Label = "South_Africa_RoofTops_CliffTops_970x250";
-	devDynamicContent[_feedName][0].Creative_Dimension = "970x250";
+	devDynamicContent[_feedName][0].Unique_ID = 30003;
+	devDynamicContent[_feedName][0].Reporting_Label = "Middle_East_RoofTops_CliffTops_728x90";
+	devDynamicContent[_feedName][0].Creative_Dimension = "728x90";
 	devDynamicContent[_feedName][0].Start_Date = "01/01/2020T00:00:00+00:00";
 	devDynamicContent[_feedName][0].End_Date = "12/31/2021T00:00:00+00:00";
 	devDynamicContent[_feedName][0].CM_PlacementID = "";
@@ -20,18 +20,18 @@ var devDynamicContent;
 
 	devDynamicContent[_feedName][0].frame01a_img = {};
 	devDynamicContent[_feedName][0].frame01a_img.Type = "file";
-	devDynamicContent[_feedName][0].frame01a_img.Url = "../_assets/img_rooftops_320x190_x1.jpg";
+	devDynamicContent[_feedName][0].frame01a_img.Url = "../_assets/img_rooftops_282x78_x2.jpg";
 	devDynamicContent[_feedName][0].frame01b_img = {};
 	devDynamicContent[_feedName][0].frame01b_img.Type = "file";
-	devDynamicContent[_feedName][0].frame01b_img.Url = "../_assets/img_clifftops_322x190_x1.jpg";
+	devDynamicContent[_feedName][0].frame01b_img.Url = "../_assets/img_clifftops_282x78_x2.jpg";
 	devDynamicContent[_feedName][0].frame01_copy01 = "TAKE OFF TO";
 	devDynamicContent[_feedName][0].frame01_copy02 = "ROOFTOPS<div>CLIFFTOPS</div>";
 	devDynamicContent[_feedName][0].frame01_icon = {};
 	devDynamicContent[_feedName][0].frame01_icon.Type = "file";
-	devDynamicContent[_feedName][0].frame01_icon.Url = "../_assets/ampersand_970x250_x2.gif";
+	devDynamicContent[_feedName][0].frame01_icon.Url = "../_assets/ampersand_728x90_x2.gif";
 	devDynamicContent[_feedName][0].frame01_frame = {};
 	devDynamicContent[_feedName][0].frame01_frame.Type = "file";
-	devDynamicContent[_feedName][0].frame01_frame.Url = "../_assets/img_frame_Jansale2021_970x250.png";
+	devDynamicContent[_feedName][0].frame01_frame.Url = "../_assets/img_frame_Jansale2021_728x90.png";
 
 	devDynamicContent[_feedName][0].frame02_copy01 = "";
 	devDynamicContent[_feedName][0].frame02_copy02 = "";
@@ -67,10 +67,10 @@ var devDynamicContent;
 	devDynamicContent[_feedName][0].frameTimes = "4,0,4";
 
 	//    0:F1a_img, 1:F1b_img, 2:F1_icon, 3:F1_1, 4:F1_2, 5:F2_1, 6:F2_2, 7:F2_i, 8:F3_1  9:F3_2  10:F3_i 11:Fe_1 12:Fe_2   13:F4_1, 14:F4_2  15:F4_p  16:cta
-	devDynamicContent[_feedName][0].showCopyDelays="0,0,.5,.5,.5,1,1,.5,.5,.5,.5,4.5,4.5,.5,.5,.5,1";
+	devDynamicContent[_feedName][0].showCopyDelays="0,0,.5,.5,.5,1,1,.5,.5,.5,.5,0,0,.5,.5,.5,1";
 
 	//   0:F1a_img, 1:F1b_img, 2:F1_icon, 3:F1_1, 4:F1_2, 5:F2_1, 6:F2_2, 7:F2_i, 8:F3_1  9:F3_2  10:F3_i 11:Fe_1 12:Fe_2   13:F4_1, 14:F4_2,  15:F4_p  16:cta
-	devDynamicContent[_feedName][0].copyAnimTypes="10.5,11.5,12,f,f,f,f,f,f,f,f,f,f,f,f,f,99";
+	devDynamicContent[_feedName][0].copyAnimTypes="10.5,11.5,12,f,f,f,f,f,f,f,f,f,f,f,f,f,99"
 
 	devDynamicContent[_feedName][0].logoBA01Frames = "true,true,true,true";
 	devDynamicContent[_feedName][0].logoBA02Frames = "false,false,false,false";
@@ -79,13 +79,13 @@ var devDynamicContent;
 
 	devDynamicContent[_feedName][0].img_logo_ba_01 = {};
 	devDynamicContent[_feedName][0].img_logo_ba_01.Type = "file";
-	devDynamicContent[_feedName][0].img_logo_ba_01.Url = "../_assets/logo_BA_Jansale2021_970x250_x2.png";
+	devDynamicContent[_feedName][0].img_logo_ba_01.Url = "../_assets/logo_BA_Jansale2021_728x90_x2.png";
 	devDynamicContent[_feedName][0].img_logo_ba_02 = {}; 
 	devDynamicContent[_feedName][0].img_logo_ba_02.Type = "file";
 	devDynamicContent[_feedName][0].img_logo_ba_02.Url = "../_assets/noImage.png";
 	devDynamicContent[_feedName][0].img_logo_partners = {};
 	devDynamicContent[_feedName][0].img_logo_partners.Type = "file";
-	devDynamicContent[_feedName][0].img_logo_partners.Url = "../_assets/logo_BA_partners_noATOL_970x250_x2.png";
+	devDynamicContent[_feedName][0].img_logo_partners.Url = "../_assets/logo_BA_partners_noATOL_728x90_x2.png";
 	devDynamicContent[_feedName][0].img_logo_extra = {};
 	devDynamicContent[_feedName][0].img_logo_extra.Type = "file"
 	devDynamicContent[_feedName][0].img_logo_extra.Url = "../_assets/noImage.png";
@@ -94,7 +94,7 @@ var devDynamicContent;
 	devDynamicContent[_feedName][0].img_replayBtn.Url = "../_assets/btnReplay_x2.png";
 
 	devDynamicContent[_feedName][0].loopingProps = "0,4,true";
-	devDynamicContent[_feedName][0].css_overrides = "#frame04_copy01, #frame03_copy02, #frame03_copy01 {color:#ce210f;} #frame01_copy01 { top: 50px; } #frame01_copy02 { top: 100px; left: -325px; font-size: 30px; letter-spacing:4.8px; line-height:1.05em;width:645px; text-align:right;} #frame01_copy02 div { left: 720px; text-align:left;} #frame01_copy01, #frame01_copy02 { text-shadow: 0 0 6px rgba(0,0,0,0.7); } #frame03_copy01{font-size:30px;letter-spacing:.15em;top:43px;left:-138px;line-height:36px} #frame03_copy01b { position: relative; left: 41px; } #frame03_copy02{width:688px;left:-142px;top:114px;font-size:40px;letter-spacing:.16em} #frame04_copy01, #frame04_copy02 { top: 75px; letter-spacing: 3.5px; font-size: 28px; line-height: 33px; left: 22px; width: 290px; } #frame04_copy02{left:134px;font-size:15px;width:504px;top:183px;letter-spacing:.127em} #frame04_copy02b{font-size:18px;top:73px;position:absolute;left:308px;letter-spacing:.24em} #frame04_copy02a1{position:absolute;left:233px}#frame04_copy02a2{position:absolute; left:535px} #cta{top:171px;width:149px;left:735px;height:34px} #cta_copy{line-height:34px;text-indent:4px} #cta_bg{border-color:#ce210f;} .ctaCopy { color:#ce210f;} #container:active #cta_bg,#container:hover #cta_bg{background:#ce210f!important;border:1px solid #ce210f!important;} .termsCopy { right: 12px; bottom: 9px; left: auto; top: auto; text-align: right; } #frame01a_img_container, #frame01b_img_container, #frame01a_img,#frame01b_img { width:320px; height:190px;} #frame01a_img_container {left:31px} #frame01b_img_container{left: 351px;} #frame01_icon{left:329px;top:88px} #frame01_icon.animateOff_F3{opacity:1!important} #frame01.animateOff{transition:opacity .5s 0s} #frame04_copy01{left:100px;width:644px;letter-spacing:.19em;top:107px;font-size:30px;text-align:left} #frame04_copy01 div{position:absolute;top:0;left:293px} #frame03_copy01 div{left:14px!important;}";
+	devDynamicContent[_feedName][0].css_overrides = "#frame04_copy01, #frame03_copy02, #frame03_copy01 {color:#ce210f;} #frame01_copy01{left:129px;text-align:left;letter-spacing:1.2px;font-size:11px;top:11px} #frame01_copy02{left:73px;letter-spacing:3.4px} #frame01_copy02 div{left:241px;letter-spacing:3.9px} #frame01_copy01, #frame01_copy02 { text-shadow: 0 0 6px rgba(0,0,0,0.7); } #frame03_copy01{font-size:11px;letter-spacing:.15em;top:-4px;left:-76px;line-height:36px} #frame03_copy01b { position: relative; left: 41px; } #frame03_copy02{width:688px;left:-95px;font-size:24px;letter-spacing:.16em;top:35px!important} #frame04_copy01, #frame04_copy02 { top: 75px; letter-spacing: 3.5px; font-size: 28px; line-height: 33px; left: 22px; width: 290px; } #frame04_copy02{left:70px;font-size:9px;width:504px;top:186px;letter-spacing:.185em} #frame04_copy02b{font-size:10px;top:31px;position:absolute;left:285px;letter-spacing:.225em} #frame04_copy02a1{position:absolute;left:233px} #frame04_copy02a2{position:absolute; left:409px} #cta{top:42px;left:610px;transform:scale(1.1)!important;width:82px} #cta_copy{line-height:24px;text-indent:1px} #cta_bg{border-color:#ce210f;} .ctaCopy { color:#ce210f;} #container:active #cta_bg,#container:hover #cta_bg{background:#ce210f!important;border:1px solid #ce210f!important;} .termsCopy { right: 8px; bottom: 6px; left: auto; text-align: right; } #frame01a_img_container,#frame01b_img_container{width:282px;height:78px;left:288px;top:6px} #frame01a_img_container{left:6px} #frame01_icon{left:279px;top:26px} #frame01_icon.animateOff_F3{opacity:1!important} #frame01.animateOff{transition:opacity .5s 0s} #frame04_copy01{left:100px;width:644px;letter-spacing:.19em;top:29px;font-size:23px;text-align:left} #frame04_copy01 div{position:absolute;top:0;left:215px} #frame03_copy01 div{left:14px!important;} sup{font-size:unset;vertical-align:unset;line-height:unset}";
 
 	try {
 		Enabler.setDevDynamicContent(devDynamicContent);
